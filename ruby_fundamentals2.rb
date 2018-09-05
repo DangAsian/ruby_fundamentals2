5cbf21b13505f8077578b266bd6c2168de6fdbe2
Daniel Ang <dan.justin.ang@gmail.com>
2018-09-05 17:04:17 -0400

    second commit

---
+++ ruby_fundamentals2.rb
@@ -0,0 +1,17 @@
+# def double(my_number)
+#
+#   return my_number * 2
+# end
+#
+# puts double(4)
+
+def negative(my_number)
+
+  if my_number >= 0
+    return false
+  else my_number < 0
+    return true
+  end
+end
+
+  puts negative(2)
